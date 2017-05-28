39c9c8155994709197df277ced402c3731aa4afc
lmislm <lmjustlm@163.com>
2017-05-28 14:11:33 +0800

    添加数据库文件
    完成银行转出和回滚

---
+++ py-sql1/pysql.sql
@@ -0,0 +1,60 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.55 : Database - pysql
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pysql` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `pysql`;
+
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `acctid` tinyint(11) unsigned DEFAULT NULL COMMENT '账户ID',
+  `money` bigint(20) unsigned DEFAULT NULL COMMENT '余额'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `account` */
+
+insert  into `account`(`acctid`,`money`) values (11,200),(13,400);
+
+/*Table structure for table `tb0` */
+
+DROP TABLE IF EXISTS `tb0`;
+
+CREATE TABLE `tb0` (
+  `userid` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb0` */
+
+insert  into `tb0`(`userid`,`username`) values (1,'name1'),(2,'name2'),(3,'name3'),(4,'name4'),(5,'name5'),(6,'name6'),(7,'name7'),(8,'name8'),(9,'name9');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userid` int(11) DEFAULT NULL,
+  `username` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
